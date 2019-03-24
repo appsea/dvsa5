170b59926af79f19f496b666bbbb39c930ffe127
appsea <girase.rakesh@gmail.com>
2019-03-24 22:47:17 +0000

    Fixed Short Imports

--- app/services/question.service.ts
+++ app/services/question.service.ts
@@ -6,7 +6,7 @@ import * as Toast from "nativescript-toast";
 import * as appSettings from "tns-core-modules/application-settings";
 import { Observable } from "tns-core-modules/data/observable";
 import * as dialogs from "tns-core-modules/ui/dialogs";
-import * as utils from "utils/utils";
+import * as utils from "tns-core-modules/utils/utils";
 import { ConnectionService } from "~/shared/connection.service";
 import { IQuestion } from "~/shared/questions.model";
 import { QuizUtil } from "~/shared/quiz.util";
