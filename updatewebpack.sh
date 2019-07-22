2874f471250fba7a453d3ef7022d7d90f475ab17
appsea <girase.rakesh@gmail.com>
2019-07-22 13:18:15 +0100

    Added node-sass

---
+++ updatewebpack.sh
@@ -0,0 +1,8 @@
+cd C:/Data/Rakesh/Workspace/Projects/Nativescript/Dvsa
+npm run lintfix
+cd C:/Data/Rakesh/Workspace/Projects/Nativescript/AdvanceSas
+npm run lintfix
+cd C:/Data/Rakesh/Workspace/Projects/Nativescript/BaseSas
+npm run lintfix
+cd C:/Data/Rakesh/Workspace/Projects/Nativescript/CompTIAA+
+npm run lintfix
